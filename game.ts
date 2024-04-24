1007811bcaba696c84f89d20266973d24d6031f1
AyeshaShahzaib <146367193+AyeshaShahzaib@users.noreply.github.com>
2024-04-24 13:24:33 +0500

    Delete file

--- game.ts
+++ game.ts
@@ -1,10 +1,1 @@
-import inquirer from "inquirer";
-const random_number= Math.floor(Math.random()*6+1)
-const guess = await inquirer.prompt([
-    {message:"Guess the correct no from 1-6 :",type:"input",name:"Guess"}
-])
-if(random_number==guess.Guess){
-    console.log("Congratulations! you guess the correct number")
-}else{
-    console.log("wrong Answer","\nThe correct answer is",random_number)
-}
+
